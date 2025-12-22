3176c16e5c79d01d77e85cdc4e0729fba86eee86
leehyoeun <s25049@gsm.hs.kr>
2025-12-22 17:55:22 +0900

    기본 세팅

---
+++ foodi-app/app/Login/page.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      <div className="flex w-full max-w-md flex-col gap-8 rounded-lg bg-white p-10 shadow-lg">
+        {/* 로고 넣을 곳 */}
+        <h2 className="text-black">Foodi</h2>
+        
+      </div>
+    </div>
+  );
+}
